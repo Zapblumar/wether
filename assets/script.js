d53a9eb8880fe3eb81090b366281f94b4d8d7d86
Tylor <Zapblumar@gmail.com>
2020-10-29 21:38:28 -0700

    got the html where I like it

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,16 @@
 var search = document.querySelector("#city-Btn");
 var APIKey = "6d5ccf5473302b19f719a739ab7ff1c2";
-var getweather = function (weather) {
+var cityName = document.querySelector(".search")
+
+
+var getweather = function () {
 
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey)
         .then(r => r.json);
 
-
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIKey)
+        .then(r => r.json);
 }
 
 search.onclick = getweather;
+
