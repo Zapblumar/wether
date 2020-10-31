39e0f7cd9eb2a2aa410e3837aa89a2bfcbf5105f
Tylor <Zapblumar@gmail.com>
2020-10-31 14:03:42 -0700

    got the fetch to work

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,23 @@
 var search = document.querySelector("#city-Btn");
-var APIKey = "6d5ccf5473302b19f719a739ab7ff1c2";
-var cityName = document.querySelector(".search")
+//var APIKey = "6d5ccf5473302b19f719a739ab7ff1c2";
+var where = document.querySelector("#current");
 
 
-var getweather = function () {
+var getweather = function (e) {
+    e.preventDefault();
+    var cityName = document.querySelector("#search").value;
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=6d5ccf5473302b19f719a739ab7ff1c2")
+        .then(r => r.json())
+        .then(function (json) {
+            console.log(json)
+        });
 
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey)
-        .then(r => r.json);
-
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIKey)
-        .then(r => r.json);
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=6d5ccf5473302b19f719a739ab7ff1c2")
+        .then(r => r.json())
+        .then(function (json) {
+            console.log(json)
+        });
+    console.log(cityName)
 }
 
 search.onclick = getweather;
